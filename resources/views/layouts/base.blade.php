12bf3b44fa6e52b91f71820025cd73deed2dd4a9
mikhail <korzinadlyamusora@gmail.com>
2019-11-15 10:46:19 +0300

    initial commit

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>App Name - @yield('title')</title>
+        <link href="css/app.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+        <script src="js/app.js"></script>
+    </body>
+</html>
---
+++ resources/views/testbootstrap.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.base')
+
+@section('title', 'Page Title')
+
+
+@section('content')
+    <div class="alert alert-primary" role="alert">
+        A simple primary alert—check it out!
+    </div>
+@endsection
