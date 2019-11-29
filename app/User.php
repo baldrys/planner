9a8ccedafa1404960453275dc93f345ea96bc2ba
mikhail <korzinadlyamusora@gmail.com>
2019-11-29 12:18:39 +0300

    Добавлен контроллер аутентификации и установлен passport

--- app/User.php
+++ app/User.php
@@ -2,17 +2,15 @@
 
 namespace App;
 
-use Carbon\Carbon;
+use Laravel\Passport\HasApiTokens;
 use App\Support\DaysPicker;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\DayActivity;
-use App\UserActivity;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
