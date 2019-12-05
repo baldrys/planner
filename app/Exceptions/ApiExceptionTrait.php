63c7ba70d39c0392a7aa3d3c25d57d4ec25a3cfc
mikhail <korzinadlyamusora@gmail.com>
2019-12-05 20:53:55 +0300

    Рефакторинг exceptions

--- app/Exceptions/ApiExceptionTrait.php
+++ app/Exceptions/ApiExceptionTrait.php
@@ -2,55 +2,49 @@
 
 namespace App\Exceptions;
 
-use Symfony\Component\HttpKernel\Exception\HttpException;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpFoundation\Response;
-use Illuminate\Validation\ValidationException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Illuminate\Auth\Access\AuthorizationException;
-use GuzzleHttp\Exception\BadResponseException;
 
 trait ApiExceptionTrait
 {
-	public function apiException($request, $exception)
-	{
-        if ($exception instanceof NotFoundHttpException) {
-            $response['message'] = "Resource not found.";
-            $statusCode = $exception->getStatusCode();
-        }
-        else if ($exception instanceof ModelNotFoundException) {
-            $ids = $exception->getIds();
-            $id = count($exception->getIds()) === 1 ? $ids[0] : $ids;
-            $s = explode("\\", $exception->getModel());
-            $model = end($s);
-            $response['message'] = "{$model} with id {$id} was not found";
-            $statusCode = Response::HTTP_NOT_FOUND;
+
+    public function apiException($request, $e)
+    {
+        if ($this->isModel($e)) {
+            return $this->modelResponse($e);
         }
 
-        else if ($exception instanceof ValidationException) {
-            $response['message'] = $exception->getMessage();
-            $response['errors'] = $exception->errors();
-            $statusCode = $exception->status;
-        } 
-        else if($exception instanceof AuthorizationException){
-            $response['message'] = $exception->getMessage();
-            $statusCode = Response::HTTP_FORBIDDEN;
+        if ($this->isHttp($e)) {
+            return $this->httpResponse($e);
         }
-        else if($exception instanceof BadResponseException){
-            $response['message'] = $exception->getMessage();
-            $statusCode = $exception->getCode();
-        }
-        else if($exception instanceof HttpException){
-            $response['message'] = $exception->getMessage();
-            $statusCode = $exception->getStatusCode();
-        }
-        else {
-            //return parent::render($request, $exception);
-            $response['message'] = $exception->getMessage();
-            $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
-        }
-        return response()->json($response, $statusCode);
+
+        return parent::render($request, $e);
     }
-    
 
+
+    protected function isModel($e)
+    {
+        return $e instanceof \Illuminate\Database\Eloquent\ModelNotFoundException;
+    }
+
+    protected function isHttp($e)
+    {
+        return $e instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+    }
+
+
+    protected function modelResponse($e)
+    {
+        $modelClass = explode('\\', $e->getModel());
+
+        return response([
+            'error' => 'Model '.end($modelClass).' not found',
+        ], Response::HTTP_NOT_FOUND);
+    }
+
+    protected function httpResponse($e)
+    {
+        return response([
+            'error' => 'Incorrect route name',
+        ], Response::HTTP_NOT_FOUND);
+    }
 }
